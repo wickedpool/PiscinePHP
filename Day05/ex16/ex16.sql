fd7715b4088c6574fc22d4b97c63f236e23d820a
Wickedpool <thgiraud@student.42.fr>
2017-04-12 14:05:23 +0200

    05-16

--- Day05/ex16/ex16.sql
+++ Day05/ex16/ex16.sql
@@ -1,1 +1,1 @@
-SELECT count(*) as film WHERE (date_debut_affiche AND date_fin_affiche BETWEEN '2006-10-30' AND '2007-07-27' OR '%-24-12' BETWEEN date_debut_affiche AND date_fin_affiche);
+SELECT COUNT(id_film) from historique_membre WHERE date > 2006-10-30 and date < 2007-07-27 or date like "%-12-24%";
